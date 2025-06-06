ea46a1e7ed6d1068eb22b3800d837704e3044bd6
Nathaniel Holden <nathaniel.holden@wpa.org.uk>
2025-06-06 10:01:28 +0100

    Created Stock and Comment Model

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5dedc058869cf9891863507404fd5bdd546f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c51c8e7cfd6a869ae0d6c232b6669791423538b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
