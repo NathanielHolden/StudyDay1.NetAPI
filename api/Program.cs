182f79250c65a87934e482c99f87b4925f8b473d
Nathaniel Holden <nathaniel.holden@wpa.org.uk>
2025-06-06 16:33:17 +0100

    Done up to step 10 on course

--- api/Program.cs
+++ api/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
